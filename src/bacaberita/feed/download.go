78c6fd2f68d90440d48ba1f445ed8433a845a7a4
Fajran Iman Rusadi <fajran@gmail.com>
2013-05-16 01:32:17 +0200

    Add file downloader

---
+++ src/bacaberita/feed/download.go
@@ -0,0 +1,18 @@
+package feed
+
+import (
+	"io/ioutil"
+	"net/http"
+)
+
+func Download(url string) ([]byte, error) {
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+
+	return data, err
+}
