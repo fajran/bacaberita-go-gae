f0fe780a2ad82be8b7f463c0f0a2136e12f6bbda
Fajran Iman Rusadi <fajran@gmail.com>
2013-05-17 21:22:21 +0200

    Use URL Fetch API to download files

---
+++ src/bacaberita/utils/download.go
@@ -0,0 +1,22 @@
+package service
+
+import (
+	"io/ioutil"
+
+	"appengine"
+	"appengine/urlfetch"
+)
+
+func Download(url string, c appengine.Context) ([]byte, error) {
+	client := urlfetch.Client(c)
+
+	resp, err := client.Get(url)
+	if err != nil {
+		return nil, err
+	}
+	defer resp.Body.Close()
+
+	data, err := ioutil.ReadAll(resp.Body)
+
+	return data, err
+}
