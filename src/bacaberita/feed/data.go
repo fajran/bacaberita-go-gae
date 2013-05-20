9c7c7d199f7bef27a7e28e105d31b3123bfdfb15
Fajran Iman Rusadi <fajran@gmail.com>
2013-05-20 16:14:04 +0200

    Cleanup data structure
    * Add date information to channel
    * Replace custom Date type with time.Time
    * Make the date attributes as pointer to time.Time since the dates can be undefined
    * Remove tags for XML and Google's App Engine Data Store

--- src/bacaberita/feed/data.go
+++ src/bacaberita/feed/data.go
@@ -4,42 +4,21 @@ import (
 	"time"
 )
 
-type Date string
-
-func (self Date) Parse() (time.Time, error) {
-	return time.Parse(time.RFC822, string(self))
-}
-
-func (self Date) Format(format string) (string, error) {
-	t, err := self.Parse()
-	if err != nil {
-		return "", err
-	}
-	return t.Format(format), nil
-}
-
-func (self Date) MustFormat(format string) string {
-	s, err := self.Format(format)
-	if err != nil {
-		return err.Error()
-	}
-	return s
-}
-
 type Item struct {
-	Title       string `xml:"title" datastore:"title"`
-	Link        string `xml:"link" datastore:"link"`
-	Guid        string `xml:"guid" datastore:"guid"`
-	Date        Date   `xml:"pubDate" datastore:"date"`
-	Description string `xml:"description" datastore:"description,noindex"`
+	Title       string
+	Link        string
+	Guid        string
+	Date        *time.Time
+	Description string
 }
 
 type Feed struct {
-	Title       string `xml:"title" datastore:"title,noindex"`
-	Link        string `xml:"link" datastore:"link"`
-	Description string `xml:"description" datastore:"description,noindex"`
-	ImageUrl    string `xml:"image>url" datastore:"image_url,noindex"`
-	ImageTitle  string `xml:"image>title" datastore:"image_title,noindex"`
-	ImageLink   string `xml:"image>link" datastore:"image_link,noindex"`
-	Items       []Item `xml:"item" datastore:"-"`
+	Title       string
+	Link        string
+	Description string
+	Date        *time.Time
+	ImageUrl    string
+	ImageTitle  string
+	ImageLink   string
+	Items       []Item
 }
