32a89d609b90e1170f20920126906f8acd647725
Fajran Iman Rusadi <fajran@gmail.com>
2013-06-10 01:45:06 +0200

    Add simple login/logout/register/update page

--- src/bacaberita/service/http.go
+++ src/bacaberita/service/http.go
@@ -5,6 +5,7 @@ import (
 	"net/http"
 
 	"appengine"
+	"appengine/user"
 
 	"bacaberita/data"
 )
@@ -12,6 +13,23 @@ import (
 func init() {
 	http.HandleFunc("/register", register)
 	http.HandleFunc("/update", update)
+	http.HandleFunc("/", main)
+}
+
+func main(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-type", "text/html; charset=utf-8")
+	c := appengine.NewContext(r)
+	u := user.Current(c)
+	if u == nil {
+		url, _ := user.LoginURL(c, "/")
+		fmt.Fprintf(w, `<a href="%s">Sign in or register</a>`, url)
+		return
+	}
+	url, _ := user.LogoutURL(c, "/")
+	fmt.Fprintf(w, `Welcome, %s! (<a href="%s">sign out</a>)<br/>`, u, url)
+
+	fmt.Fprintf(w, `<form method="post" action="/register">Register <input type="text" name="url"/><input type="submit"/></form>`)
+	fmt.Fprintf(w, `<form method="post" action="/update">Update <input type="text" name="url"/><input type="submit"/></form>`)
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
