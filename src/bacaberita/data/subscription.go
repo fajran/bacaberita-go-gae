9b2fa7df896f7f47dacfe972e86f726000d3dd39
Fajran Iman Rusadi <fajran@gmail.com>
2013-06-10 02:02:43 +0200

    Allow users to subscribe to feeds
    Closes #17

---
+++ src/bacaberita/data/subscription.go
@@ -0,0 +1,65 @@
+package data
+
+import (
+	"time"
+
+	"appengine"
+	"appengine/datastore"
+	"appengine/user"
+)
+
+type SubscriptionFeed struct {
+	FeedKey *datastore.Key
+	Created time.Time
+}
+
+type Subscription struct {
+	UserID   string
+	FeedList []SubscriptionFeed
+
+	Created time.Time
+	Updated time.Time
+}
+
+func SubscribeFeed(c appengine.Context, u *user.User, feed *datastore.Key) (*datastore.Key, *Subscription, error) {
+	s := new(Subscription)
+	s.UserID = u.ID
+
+	key := s.NewKey(c)
+	err := datastore.Get(c, key, s)
+
+	item := SubscriptionFeed{}
+	item.FeedKey = feed
+	item.Created = time.Now()
+
+	if err == datastore.ErrNoSuchEntity {
+		s.FeedList = []SubscriptionFeed{item}
+		s.Created = time.Now()
+	} else if err == nil {
+		if !s.IsSubscribed(c, feed) {
+			s.FeedList = append(s.FeedList, item)
+		}
+	} else {
+		return nil, nil, err
+	}
+
+	s.UserID = u.ID
+	s.Updated = time.Now()
+
+	key, err = datastore.Put(c, key, s)
+	return key, s, err
+}
+
+func (s *Subscription) NewKey(c appengine.Context) *datastore.Key {
+	return datastore.NewKey(c, "Subscription", s.UserID, 0, nil)
+}
+
+func (s *Subscription) IsSubscribed(c appengine.Context, feed *datastore.Key) bool {
+	id := feed.StringID()
+	for _, item := range s.FeedList {
+		if id == item.FeedKey.StringID() {
+			return true
+		}
+	}
+	return false
+}
--- src/bacaberita/service/http.go
+++ src/bacaberita/service/http.go
@@ -13,6 +13,7 @@ import (
 func init() {
 	http.HandleFunc("/register", register)
 	http.HandleFunc("/update", update)
+	http.HandleFunc("/subscribe", subscribe)
 	http.HandleFunc("/", main)
 }
 
@@ -30,6 +31,7 @@ func main(w http.ResponseWriter, r *http.Request) {
 
 	fmt.Fprintf(w, `<form method="post" action="/register">Register <input type="text" name="url"/><input type="submit"/></form>`)
 	fmt.Fprintf(w, `<form method="post" action="/update">Update <input type="text" name="url"/><input type="submit"/></form>`)
+	fmt.Fprintf(w, `<form method="post" action="/subscribe">Subscribe <input type="text" name="url"/><input type="submit"/></form>`)
 }
 
 func register(w http.ResponseWriter, r *http.Request) {
@@ -91,5 +93,44 @@ func update(w http.ResponseWriter, r *http.Request) {
 		fmt.Fprintf(w, "- Key: %w\n", key)
 		fmt.Fprintf(w, "- Created: %w\n", feed.Created)
 		fmt.Fprintf(w, "- Updated: %w\n", feed.Updated)
+	}
+}
+
+func subscribe(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, "Use POST to subscribe to a feed", http.StatusMethodNotAllowed)
+		return
+	}
+
+	// FIXME figure out how to use PostFormValue instead of FormValue
+	url := r.FormValue("url")
+	if len(url) == 0 {
+		http.Error(w, "Feed URL is not specified", http.StatusBadRequest)
+		return
+	}
+
+	c := appengine.NewContext(r)
+	key, _, err := data.RegisterFeed(c, url)
+	if err != nil {
+		c.Errorf("Error when registering feed: %w", err)
+		http.Error(w, "Error when registering feed", http.StatusInternalServerError)
+		return
+	}
+
+	u := user.Current(c)
+	key, s, err := data.SubscribeFeed(c, u, key)
+
+	fmt.Fprintf(w, "Subscribe:\n")
+	fmt.Fprintf(w, "- Url: %s\n", url)
+	fmt.Fprintf(w, "- UserID: %s\n", u.ID)
+	fmt.Fprintf(w, "- Email: %s\n", u.Email)
+
+	if err != nil {
+		fmt.Fprintf(w, "- Error: %w\n", err)
+	} else {
+		fmt.Fprintf(w, "- Key: %w\n", key)
+		fmt.Fprintf(w, "- Created: %w\n", s.Created)
+		fmt.Fprintf(w, "- Updated: %w\n", s.Updated)
+		fmt.Fprintf(w, "- Feeds: %d\n", len(s.FeedList))
 	}
 }
